19dcdcc5768215fa6c091b3fea1c7dcbaced9128
IrinkaCh <123irina00@mail.ru>
2022-08-07 13:00:12 +0500

    Task 002 Max Of 3

---
+++ Task002_MaxOf3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+//int max = a;
+//if(a>b){max = a;}
+//else{max = b;}
+//if(b>c){max = b;}
+//else{max = c;}
+//if(c>a){max = c;}
+//else{max = a;}
+
+int max = a;
+if(a<b){max = b;}
+if(max<c){max = c;}
+
+Console.Write("Максимальное число найдено, max = ");
+Console.WriteLine(max);
