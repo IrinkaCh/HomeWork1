31ecf75c7cac5d73f3aaa94b236515b4ade2409f
IrinkaCh <123irina00@mail.ru>
2022-08-07 12:07:23 +0500

    Task 001 Min Max

---
+++ Task001_MinMax/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 0;
+
+if(a>b)
+{
+max = a;
+min = b;
+}
+else
+{
+max = b;
+min = a;
+}
+Console.Write("Максимальное число найдено, max = ");
+Console.WriteLine(max);
+Console.Write("Минимальное число найдено, min = ");
+Console.WriteLine(min);
