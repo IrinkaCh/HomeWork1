20fc4be22d04d3cf96ae949158d122dfc7d3db7f
IrinkaCh <123irina00@mail.ru>
2022-08-07 13:19:26 +0500

    Task 003 Multiple 2

---
+++ Task003_Multiple2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Является ли число чётным?!");
+Console.WriteLine("Введите любое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n % 2 == 0)
+{Console.WriteLine("ДА!");}
+else{Console.WriteLine("нет");}
