4eb468169f84d7fa902a589c0a9b2ed94b0d980a
IrinkaCh <123irina00@mail.ru>
2022-08-08 15:15:00 +0500

    Task 001 min max

--- Task001_MinMax/Program.cs
+++ Task001_MinMax/Program.cs
@@ -9,3 +9,14 @@ if(a>b){max = a; min = b;}
 
 Console.WriteLine("Максимальное число найдено, max = " + max);
 Console.WriteLine("Минимальное число найдено, min = " + min);
+
+// Вопрос по первой задаче: "..и выдаёт, какое число большее, а какое меньшее.", но в примере показано только вывод максимального. 
+// решение для задачи, которая указана в примере:
+//Console.WriteLine("Введите число a ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число b ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//if(a>b){Console.WriteLine("Максимальное число найдено, max = " + a);}
+//else{Console.WriteLine("Максимальное число найдено, max = " + b);}
+
+
